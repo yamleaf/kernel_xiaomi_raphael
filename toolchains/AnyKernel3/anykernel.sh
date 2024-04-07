4784a6909b622a2f0421edc0174ae71ea1360d54
yamleaf <57785628+yamleaf@users.noreply.github.com>
2024-04-07 09:25:21 +0800

    anykernel脚本适配erofs及ext4格式内核

--- toolchains/AnyKernel3/anykernel.sh
+++ toolchains/AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=rikka
+kernel.string=RikkaKernel V2
 do.devicecheck=1
 do.modules=0
 do.systemless=0
@@ -22,6 +22,15 @@ is_slot_device=0;
 ramdisk_compression=auto;
 no_block_display=true;
 patch_vbmeta_flag=auto;
+
+if [[ -f Image.gz-dtb.erofs ]] || [[ -f Image.gz-dtb.ext4 ]]; then
+    var=$(awk '/\/vendor erofs/ {print $3}' /proc/mounts)
+    if [[ ${var} = "erofs" ]]; then
+        mv Image.gz-dtb.erofs Image.gz-dtb
+    else
+        mv Image.gz-dtb.ext4 Image.gz-dtb
+    fi
+fi
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
